417a1b350adf23f2f8cfb09256d3aad0d7a86b6c
Aghanash Karthik <aghanashkarthik@Karthiks-MacBook-Pro.local>
2018-08-14 21:14:54 -0400

    Added comments with the README

--- roles/service-deploy-devops-web/files/apache_include/artifacts_footer.php
+++
@@ -1,8 +0,0 @@
-   </td>
-  </tr>
-  <tr>
-    <td colspan='2'><center>
-       <address>iDirect Artifact Repostiory</address>
-    </center></td>
-  </tr>
-</table>
--- roles/swarm.sh
+++ roles/swarm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-java -jar /home/jenkins/swarm-client-2.0-jar-with-dependencies.jar -name "slave" -password "iDirect" -username "admin" -master http://192.168.94.184:8080 -disableSslVerification  &> /home/jenkins/swarm-client.log &
+# This file is used to create a jenkins slave with the swarm client with the username and password
+# for the server as mentioned
+java -jar /home/jenkins/swarm-client-2.0-jar-with-dependencies.jar -name "slave" -password "password" -username "admin" -master server "server-ip" -disableSslVerification  &> /home/jenkins/swarm-client.log &
